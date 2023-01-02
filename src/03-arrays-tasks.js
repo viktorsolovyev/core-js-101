b5182941daedbee760a86ba34a54029332d63021
Viktor Solovyev <barn-mih@rambler.ru>
2023-01-02 20:08:42 +0300

    feat: done some tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -489,8 +489,8 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return new Array(...new Set(arr));
 }
 
 /**
